4db43ed9b54fdf6b843465a36a99d019292b8644
Mustafa Abdullahbhai Khorakiwala <mustafakhorakiwala1@gmail.com>
2021-12-09 19:21:29 +0530

    Added Fragments

--- app/src/main/java/com/example/pupilsfacebook/MainActivity.java
+++ app/src/main/java/com/example/pupilsfacebook/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.pupilsfacebook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.pupilsfacebook.Fragments.AddFragment;
+import com.example.pupilsfacebook.Fragments.HomeFragment;
+import com.example.pupilsfacebook.Fragments.NotificationFragment;
+import com.example.pupilsfacebook.Fragments.ProfileFragment;
+import com.example.pupilsfacebook.Fragments.SearchFragment;
+import com.example.pupilsfacebook.databinding.ActivityMainBinding;
+import com.iammert.library.readablebottombar.ReadableBottomBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, new HomeFragment());
+        transaction.commit();
+
+        binding.readableBottomBar.setOnItemSelectListener(new ReadableBottomBar.ItemSelectListener() {
+            @Override
+            public void onItemSelected(int i) {
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                switch(i){
+                    case 0:
+                        transaction.replace(R.id.container, new HomeFragment());
+                        break;
+                    case 1:
+                        transaction.replace(R.id.container, new NotificationFragment());
+                        break;
+                    case 2:
+                        transaction.replace(R.id.container, new AddFragment());
+                        break;
+                    case 3:
+                        transaction.replace(R.id.container, new SearchFragment());
+                        break;
+                    case 4:
+                        transaction.replace(R.id.container, new ProfileFragment());
+                        break;
+                }
+                transaction.commit();
+            }
+        });
     }
 }
